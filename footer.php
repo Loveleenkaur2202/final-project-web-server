ee4174af72991857c0c0550ba0d01215f6cf3bcc
Loveleenkaur2202 <loveleenkaurr2002@gmail.com>
2023-04-12 22:17:10 -0400

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,6 +1,6 @@
 <footer>
     © 2023 - <?php echo date("Y");
-    echo "<br/>Developed by <br/>Hebert Pierre Canel<br/>RishabhSareen<br/>Loveleen Kaur"
+    echo "<br/>Developed by <br/>Hebert Pierre Canel<br/>Rishabh Sareen<br/>Loveleen Kaur"
     ?>
     </footer>
 </div>
